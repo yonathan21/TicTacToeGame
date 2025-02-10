058c50d32432f6545086aac2c91b6e93efeb8d1c
yonathan21 <eldanadereje06@gmail.com>
2025-02-11 00:19:04 +0300

    addingonclickatt

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const selectBox = document.querySelector(".select-box"),
 selectXBtn = selectBox.querySelector(".playerX"),
 selectOBtn = selectBox.querySelector(".playerO"),
-playBoard = document.querySelector(".play-board");
+playBoard = document.querySelector(".play-board"),
+allBox = document.querySelectorAll("section span")
 
 window.onload = () => {
+  for(let i = 0; i < allBox.length; i++){
+    allBox[i].setAttribute('onclick', 'clickedBox(this)');
+  }
+
   selectXBtn.onclick = () => {
     selectBox.classList.add("hide");
     playBoard.classList.add("show");
