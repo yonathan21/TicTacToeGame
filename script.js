b346dc074cd0abe8f5033ad265bff61f825b7070
yonathan21 <eldanadereje06@gmail.com>
2025-02-11 20:51:04 +0300

    addingid

--- script.js
+++ script.js
@@ -25,15 +25,19 @@ window.onload = () => {
 
 let playerXIcon = "fas fa-times";
 let playerOIcon = "far fa-circle";
+let playerSign = 'X';
 
 function clickedBox(element){
   
   if(players.classList.contains("player")){
     element.innerHTML =  `<i class = "${playerOIcon}"></i>`;
     players.classList.add('active');
+    playerSign = 'O';
+    element.setAttribute('id', playerSign);
   }else{
     element.innerHTML =  `<i class = "${playerXIcon}"></i>`;
     players.classList.add('active');
+    element.setAttribute('id', playerSign);
   }
 
   element.style.pointerEvents = "none";
@@ -46,7 +50,7 @@ function clickedBox(element){
 }
 
 function bot(){
-
+  playerSign = 'O';
   let array =[];
 
   for(let i = 0; i < allBox.length; i++){
@@ -58,13 +62,18 @@ function bot(){
   let randomBox = array[Math.floor(Math.random() * array.length)];
   
   if(array.length > 0){
+
     if(players.classList.contains("player")){
       allBox[randomBox].innerHTML =  `<i class = "${playerXIcon}"></i>`;
       players.classList.remove('active');
+      playerSign = 'X';
+      allBox[randomBox].setAttribute('id', playerSign);
     }else{
       allBox[randomBox].innerHTML =  `<i class = "${playerOIcon}"></i>`;
       players.classList.remove('active');
+      allBox[randomBox].setAttribute('id', playerSign);
     }
+
   }
   
   allBox[randomBox].style.pointerEvents = 'none';
