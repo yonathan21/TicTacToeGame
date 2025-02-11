baf8005d7b22f94441a8cc48eda5fd9a30103640
yonathan21 <eldanadereje06@gmail.com>
2025-02-11 12:08:39 +0300

    changingxturnandoturn

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+
+
 const selectBox = document.querySelector(".select-box"),
 selectXBtn = selectBox.querySelector(".playerX"),
 selectOBtn = selectBox.querySelector(".playerO"),
 playBoard = document.querySelector(".play-board"),
-allBox = document.querySelectorAll("section span")
+allBox = document.querySelectorAll("section span"),
+players = document.querySelector(".players");
 
 window.onload = () => {
   for(let i = 0; i < allBox.length; i++){
@@ -16,5 +19,17 @@ window.onload = () => {
   selectOBtn.onclick = () => {
     selectBox.classList.add("hide");
     playBoard.classList.add("show");
+    players.setAttribute('class', "players active player");
+  }
+}
+
+let playerXIcon = "fas fa-times";
+let playerOIcon = "far fa-circle";
+
+function clickedBox(element){
+  if(players.classList.contains("player")){
+    element.innerHTML =  `<i class = "${playerOIcon}"></i>`;
+  }else{
+    element.innerHTML =  `<i class = "${playerOIcon}"></i>`;
   }
 }
