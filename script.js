0b2c36cdf02f2c7a3b56c419952d765718e6c373
yonathan21 <eldanadereje06@gmail.com>
2025-02-11 21:19:41 +0300

    selectingwinner

--- script.js
+++ script.js
@@ -40,8 +40,10 @@ function clickedBox(element){
     element.setAttribute('id', playerSign);
   }
 
+  selectWinner();
+  playBoard.style.pointerEvents = 'none';
   element.style.pointerEvents = "none";
-
+  
   let randomDelayTime = (Math.random() * 1000) + 200;
   setTimeout(() => {
     bot();
@@ -73,8 +75,26 @@ function bot(){
       players.classList.remove('active');
       allBox[randomBox].setAttribute('id', playerSign);
     }
-
+      selectWinner();
   }
   
   allBox[randomBox].style.pointerEvents = 'none';
+  playBoard.style.pointerEvents = 'auto';
+  playerSign = 'X';
+}
+
+function getClass(idname){
+  return document.querySelector('.box' + idname).id;
+}
+
+function checkThreeClasses(val1,val2,val3,sign){
+  if(getClass(val1) == sign && getClass(val2) == sign && getClass(val3) == sign){
+    return true;
+  }
+}
+
+function selectWinner(){
+  if(checkThreeClasses(1,2,3,playerSign) || checkThreeClasses(4,5,6,playerSign) || checkThreeClasses(7,8,9,playerSign) || checkThreeClasses(1,4,7,playerSign) || checkThreeClasses(2,5,8,playerSign) || checkThreeClasses(3,6,9,playerSign) || checkThreeClasses(1,5,9,playerSign) || checkThreeClasses(3,5,7,playerSign)){
+    console.log(playerSign + " " + "is the winner");
+  }
 }
